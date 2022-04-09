4b1f7ba5e91ecc5b7e8ae3a755667e191e4c4728
xdimension <xdim222@gmail.com>
2022-04-09 22:50:51 +0700

    create Thread resources

---
+++ app/Http/Controllers/ThreadController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Thread;
+use Illuminate\Http\Request;
+
+class ThreadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $threads = Thread::all();
+
+        return $threads;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required'],
+            'slug' => ['required', 'unique:threads'],
+            'user_id' => ['required', 'exists:users,id'],
+        ]);
+            
+        $thread = Thread::create($request->validated());
+
+        return $thread;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Thread  $thread
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Thread $thread)
+    {
+        return $thread;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Thread  $thread
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Thread $thread)
+    {
+        $request->validate([
+            'title' => ['required'],
+            'slug' => ['required', 'unique:threads,slug,' . $thread->id],
+            'user_id' => ['required', 'exists:users,id'],
+        ]);
+
+        $thread->update($request->validated());
+
+        return $thread;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Thread  $thread
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Thread $thread)
+    {
+        $thread->delete();
+
+        return response()->json('thread-deleted', 204);
+    }
+}
