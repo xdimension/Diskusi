4f8ade33d5ab4d140c736848bf27166bc245ea22
xdimension <xdim222@gmail.com>
2022-04-09 21:39:47 +0700

    fixed require auth route file

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/dashboard', function () {
     return Inertia::render('Admin/Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require('../routes/auth.php');
+require __DIR__.'/auth.php';
