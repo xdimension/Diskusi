1b9e7b128ddc39c701ee026dbed551aa549cb37c
xdimension <xdim222@gmail.com>
2022-04-12 13:10:50 +0700

    Creating User Model and Resource

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
