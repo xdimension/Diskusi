0670a6cd54d432a070f94627cdff5c9ebe32d931
xdimension <xdim222@gmail.com>
2022-04-12 23:34:27 +0700

    Set layout

--- resources/Pages/Admin/Tables.vue
+++ resources/Pages/Admin/Tables.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="flex flex-wrap mt-4">
-    <div class="w-full mb-12 px-4">
-      <card-table />
+  <AdminLayout>
+    <div class="flex flex-wrap mt-4">
+      <div class="w-full mb-12 px-4">
+        <card-table />
+      </div>
+      <div class="w-full mb-12 px-4">
+        <card-table color="dark" />
+      </div>
     </div>
-    <div class="w-full mb-12 px-4">
-      <card-table color="dark" />
-    </div>
-  </div>
+  </AdminLayout>
 </template>
 <script>
 import CardTable from "@/components/Cards/CardTable.vue";
+import AdminLayout from "@/layouts/Admin.vue";
 
 export default {
   components: {
     CardTable,
+    AdminLayout
   },
 };
 </script>
